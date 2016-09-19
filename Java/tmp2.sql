9af4bd7dc604d8194baa5863fb3e1ce67d4c63b9
bluhuu <bluhuu@gmail.com>
2016-09-19 13:57:05 +0800

    kettle

---
+++ Java/tmp2.sql
@@ -0,0 +1,9 @@
+/user/data/EAI_DELIVERYLINE
+/user/data/AD_SERVER
+/user/data/AD_Org
+/user/data/C_Invoice
+/user/data/C_InvoiceLine
+/user/data/C_BPartner
+/user/data/M_Product
+/user/data/EAI_BPARTNER
+/user/data/EAI_PRODUCT
