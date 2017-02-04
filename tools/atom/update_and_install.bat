1be0c0434791c9fe8dd75971ee93f64dd74ba3ab
bluhuu <bluhuu@gmail.com>
2017-02-04 20:42:28 +0800

    a

--- tools/atom/update_and_install.bat
+++ tools/atom/update_and_install.bat
@@ -1,2 +1,2 @@
-@dir *. /b >list.dat && @(for /f %%a in (list.dat) do @( if exist %%a @( pushd %%a && cd >>../log.txt && git fetch --all >>../log.txt && git reset --hard origin/master >>../log.txt && npm i >>../log.txt && popd ))) && ( if exist list.dat del list.dat /f /q ) &&  ( if exist log.txt del log.txt /f /q ) 
+@dir *. /b >list.dat && @(for /f %%a in (list.dat) do @( if exist %%a @( pushd %%a && cd >>../log.txt && cd && git fetch --all >>../log.txt && git reset --hard origin/master >>../log.txt && npm i >>../log.txt && popd ))) && ( if exist list.dat del list.dat /f /q ) &&  ( if exist log.txt del log.txt /f /q )
 pause
