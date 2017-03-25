cbfded8377b392f563b5381e192dceb63508be81
bluhuu <bluhuu@gmail.com>
2017-03-25 22:23:42 +0800

    a

--- JavaScript/login_remember_me.js
+++ JavaScript/login_remember_me.js
@@ -1,23 +1,32 @@
 var Login = {
-write:function (name,pd){
-	$.cookie("usn",name,{expires:30,path:"/"});
-	$.cookie("usp",pd,{expires:30,path:"/"});
-},
-//读取cookied历史记录
-read:function (){
-	var n=$.cookie("usn");
-	var p=$.cookie("usp");
-	if(n&&n.length >0){
-    	$('#username').val(n);
-		$('#remember_me').prop('checked',true);
-	}
-	if(n&&n.length >0&&p&&p.length >0){
-    	$('#password').val(p);
-	}
-},
-clear : function ()
-{
-	$.cookie("usn",null,{path:"/"});
-	$.cookie("usp",null,{path:"/"});
-}
+    write: function(name, pd) {
+        $.cookie("usn", name, {
+            expires: 30,
+            path: "/"
+        });
+        $.cookie("usp", pd, {
+            expires: 30,
+            path: "/"
+        });
+    },
+    //读取cookied历史记录
+    read: function() {
+        var n = $.cookie("usn");
+        var p = $.cookie("usp");
+        if (n && n.length > 0) {
+            $('#username').val(n);
+            $('#remember_me').prop('checked', true);
+        }
+        if (n && n.length > 0 && p && p.length > 0) {
+            $('#password').val(p);
+        }
+    },
+    clear: function() {
+        $.cookie("usn", null, {
+            path: "/"
+        });
+        $.cookie("usp", null, {
+            path: "/"
+        });
+    }
 };
