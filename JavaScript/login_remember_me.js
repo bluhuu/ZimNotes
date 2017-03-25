49f2be54a7a4fe9da98dcb41e1aa7299e1ef3d66
bluhuu <bluhuu@gmail.com>
2017-03-25 16:01:43 +0800

    a

--- JavaScript/login_remember_me.js
+++ JavaScript/login_remember_me.js
@@ -1,12 +1,12 @@
 var Login = {
 write:function (name,pd){
-	$.cookie("usn",name,{expires:30,path:"/"}); 
-	$.cookie("usp",pd,{expires:30,path:"/"}); 
+	$.cookie("usn",name,{expires:30,path:"/"});
+	$.cookie("usp",pd,{expires:30,path:"/"});
 },
-//读取cookied历史记录 
-read:function (){ 
-	var n=$.cookie("usn"); 
-	var p=$.cookie("usp"); 
+//读取cookied历史记录
+read:function (){
+	var n=$.cookie("usn");
+	var p=$.cookie("usp");
 	if(n&&n.length >0){
     	$('#username').val(n);
 		$('#remember_me').prop('checked',true);
@@ -14,10 +14,10 @@ read:function (){
 	if(n&&n.length >0&&p&&p.length >0){
     	$('#password').val(p);
 	}
-}, 
-clear : function () 
-{ 
-	$.cookie("usn",null,{path:"/"}); 
-	$.cookie("usp",null,{path:"/"}); 
-} 
+},
+clear : function ()
+{
+	$.cookie("usn",null,{path:"/"});
+	$.cookie("usp",null,{path:"/"});
+}
 };
