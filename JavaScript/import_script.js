e5cb963f49205c62e29b98f1d8b0a93c26f634bc
bluhuu <bluhuu@gmail.com>
2017-04-06 17:07:06 +0800

    005

--- JavaScript/import_script.js
+++ JavaScript/import_script.js
@@ -1,7 +1,26 @@
-﻿var _hmt = _hmt || [];
-(function () {
+﻿(function () {
     var hm = document.createElement("script");
     hm.src = "//hm.baidu.com/hm.js?05bde3ad80f2ae0330d9f88900f3e4e1";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
+console.log(obj)
+console.log(o);
+o.css('attribute', 'value')
+const  = ;
+alert(msg)
+const  =
+    con
+const  = ;
+const  =
+    confirm(msg)
+alert(o)
+function
+'use strict'
+function
+
+
+
+
+
+
