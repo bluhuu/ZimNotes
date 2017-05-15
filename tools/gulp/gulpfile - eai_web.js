aeada149bfbbe20ec6abe6b346f83231713a3cfa
bluhuu <bluhuu@gmail.com>
2017-05-15 17:06:38 +0800

    RN

---
+++ tools/gulp/gulpfile - eai_web.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var proxy = require('http-proxy-middleware');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('default',function(){
+	// 代理配置, 实现环境切换
+	var proxy1 = proxy(['/elink_eai_web'], {target: 'http://192.168.20.206:8080/', changeOrigin: true});
+	var proxy2 = proxy(['/DataTablesCustom'], {target: 'http://192.168.20.206:8080/', changeOrigin: true});
+	var proxy3 = proxy(['/elink_scm_mall'], {target: 'http://192.168.20.206:8080/', changeOrigin: true});
+	// 'http://192.168.20.206:8080/elink_eai_web/v1/open'
+	browserSync.init({
+		// notify:false, //关闭页面通知
+		server: {
+			baseDir: './',
+			port: 3000,
+			middleware: [proxy1,proxy2,proxy3]
+		}
+	});
+	gulp.watch("app/asset/css/*.css").on('change', reload);
+	gulp.watch("app/*.html").on('change', reload);
+	gulp.watch("app/asset/js/*.js").on('change', reload);
+});
