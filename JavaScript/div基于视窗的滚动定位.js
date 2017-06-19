8d9001c18b44417e17b01b0242fd52432e86485a
bluhuu <bluhuu@gmail.com>
2017-06-19 15:29:37 +0800

    div基于视窗的滚动定位

---
+++ JavaScript/div基于视窗的滚动定位.js
@@ -0,0 +1,42 @@
+//函数用法
+function getTopOfWindow(id) {
+  var box = document.getElementById(id).getBoundingClientRect();
+  return box.top;
+}
+
+function setFixedInWindow(id, top) {
+  if(top<0){
+    document.getElementById(id).style.position = "fixed";
+    document.getElementById(id).style.top = "0";
+  }else{
+    document.getElementById(id).style.position = "static";
+  }
+}
+window.onscroll = function() {
+  var top = getTopOfWindow("parentId");//父窗口
+  setRelativeForWindow("id", top);//当前窗口
+};
+
+//对象用法
+function fixedOnScroll(parentId,childId) {
+  var that    = this;
+  this.parentId  = parentId;
+  this.childId    = childId;
+
+  window.onscroll = function() {
+    var top = that.getTopOfWindow(parentId);//父窗口
+    that.setFixedInWindow(childId, top);//当前窗口
+  };
+}
+fixedOnScroll.prototype.getTopOfWindow = function(id) {
+  var box = document.getElementById(id).getBoundingClientRect();
+  return box.top;
+}
+fixedOnScroll.prototype.setFixedInWindow = function(id, top) {
+  if(top<0){
+    document.getElementById(id).style.position = "fixed";
+    document.getElementById(id).style.top = "0";
+  }else{
+    document.getElementById(id).style.position = "static";
+  }
+}
